3d561cf5a604f9617b886281c0942c3eeef471f9
trome23 <romennoodles23@yahoo.com>
2019-06-14 18:34:32 -0600

    Article schema created

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
 var axios = require("axios");
+var logger = require("morgan")
+
+var bodyParser = require("body-parser")
 
 var express = require("express");
 var app = express()
 
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended:false
+    })
+);
+
 app.use(express.static("public"));
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+mongoose.connect("mongodb://localhost/scraper_news")
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+    console.log("Connected to Mongoose!")
+})
 
 app.listen(3000, function() {
     console.log("App running on port 3000!");
